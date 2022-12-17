b86c9b5b9633edaea3d621683fea96f42471a4d6
Madhav V Rajeev <75824625+GodOfCoding1@users.noreply.github.com>
2022-12-17 18:54:45 +0530

    added tickets service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,7 +6,6 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
-
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must be defined");
   }
